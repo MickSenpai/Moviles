20d6567a47355e609775848d8fba699d33a4f39e
MickSenpai <mick967@hotmail.com>
2026-02-13 10:05:49 -0800

    #Paused

--- pedo/lib/main.dart
+++ pedo/lib/main.dart
@@ -24,11 +24,55 @@ class HomePage extends StatefulWidget {
 class _HomePageState extends State<HomePage> {
 
   int _pasos = 0;
-  int? _pasos_iniciales; //historial + pasos actuales
+  int? _pasos_iniciales; //(historial + pasos actuales) - historial:inicial
   String _estado = "Waiting for...";
   Stream<StepCount>? _stepCountStream;
-  Stream<PedestrianStatus>? _pedestrianStatus;
+  Stream<PedestrianStatus>? _pedestrianStatusStream;
 
+  @override
+  void initState() { 
+    super.initState();
+    _initPermissions();
+  }
+
+  Future<void> _initPermissions() async {
+    var status = await Permission.activityRecognition.request();
+
+    if(status.isGranted){
+      _initPedometer();
+    } else {
+      setState(() {
+        _estado = "Permission Denied, Nigga";
+      });
+    }
+  }
+
+  void _initPedometer(){
+    _stepCountStream = Pedometer.stepCountStream;
+    _pedestrianStatusStream = Pedometer.pedestrianStatusStream;
+
+    _stepCountStream!.listen(_onStepCount, onError: _onStepCountError);
+
+    _pedestrianStatusStream!.listen(_onPedestrianStatusChanged, onError: _onPedestrianStatusError);
+  }
+
+  void _onStepCount(){
+
+  }
+
+  void _onStepCountError(){
+
+  }
+
+  void _onPedestrianStatusChanged(){
+
+  }
+
+  void _onPedestrianStatusError(){
+
+  }
+
+  
   @override
   Widget build(BuildContext context) {
     return Scaffold(
